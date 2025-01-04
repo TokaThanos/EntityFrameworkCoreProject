ae6e74062d4493ea8f0eab6818b7973cb8051b93
Parthasarathi Patra <59698715+TokaThanos@users.noreply.github.com>
2025-01-04 17:48:47 +0530

    migrations added (#3)

---
+++ EntityFrameworkCore.Data/Migrations/20250104121528_InitialMigrationWithSeededData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EntityFrameworkCore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigrationWithSeededData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coaches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coaches", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Teams",
+                columns: new[] { "Id", "CreatedDate", "Name" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2025, 1, 4, 12, 15, 27, 995, DateTimeKind.Unspecified).AddTicks(281), "Manchester United" },
+                    { 2, new DateTime(2025, 1, 4, 12, 15, 27, 995, DateTimeKind.Unspecified).AddTicks(1173), "F.C. Barcelona" },
+                    { 3, new DateTime(2025, 1, 4, 12, 15, 27, 995, DateTimeKind.Unspecified).AddTicks(1180), "Juventus" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coaches");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+        }
+    }
+}
