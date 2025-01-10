2c4c4417f0992f92134233b77457bdcea4f910f5
Parthasarathi Patra <parthasarathipatra10@gmail.com>
2025-01-10 19:49:30 +0530

    Query the database using EF Core - Done

---
+++ EntityFrameworkCore.Console/TeamInfoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Console
+{
+    internal class TeamInfoDTO
+    {
+        public DateTime CreatedDate { get; set; }
+        public int TeamCount { get; set; }
+    }
+}
