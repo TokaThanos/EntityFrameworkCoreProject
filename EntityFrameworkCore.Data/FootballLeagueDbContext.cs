03a70b41dcc28c804ddb387df16b4aa969d46123
Parthasarathi Patra <59698715+TokaThanos@users.noreply.github.com>
2025-04-08 23:15:14 +0530

    Db moved to sql server (#7)

--- EntityFrameworkCore.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -7,24 +7,16 @@ namespace EntityFrameworkCore.Data;
 
 public class FootballLeagueDbContext : DbContext
 {
-    public FootballLeagueDbContext()
-    {
-        var folder = Environment.SpecialFolder.LocalApplicationData;
-        var path = Environment.GetFolderPath(folder);
-        DbPath = Path.Combine(path, "FootballLeague_EfCore.db");
-    }
     public DbSet<Team> Teams { get; set; }
     public DbSet<Coach> Coaches { get; set; }
 
-    public string DbPath { get; private set; }
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite($"Data Source={DbPath}")
+        var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+        optionsBuilder.UseSqlServer($"Server=localhost,1433;Database=FootballLeague_EfCore;User Id=sa;Password={password};TrustServerCertificate=True;")
             .LogTo(Console.WriteLine, LogLevel.Information)
             .EnableSensitiveDataLogging()
             .EnableDetailedErrors();
-        optionsBuilder.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning)); // this line is added to suppress a bug present in ef core 9.0
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
