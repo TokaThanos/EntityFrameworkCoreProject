d7a908af82b91ac0245a2c3ab351eca6bb6d13e9
Parthasarathi Patra <59698715+TokaThanos@users.noreply.github.com>
2025-05-22 19:51:21 +0530

    Intermediate commit, can break things (#11)
    Caution! Can break things.

---
+++ EntityFrameworkCore.Data/Configurations/CoachConfiguration.cs
@@ -0,0 +1,18 @@
+using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EntityFrameworkCore.Data.Configurations
+{
+    public class CoachConfiguration : IEntityTypeConfiguration<Coach>
+    {
+        public void Configure(EntityTypeBuilder<Coach> builder)
+        {
+            builder.HasData(
+                new Coach { Id = 1, Name = "Ruben Amorim" },
+                new Coach { Id = 2, Name = "Carlo Ancelotti" },
+                new Coach { Id = 3, Name = "Hansi Flick" }
+            );
+        }
+    }
+}
