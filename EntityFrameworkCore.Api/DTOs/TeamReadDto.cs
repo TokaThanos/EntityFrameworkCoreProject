e0d8f53c6535e8f4f36e1ebeee826421f2730614
Parthasarathi Patra <59698715+TokaThanos@users.noreply.github.com>
2025-06-10 13:38:35 +0530

    Implemented teams CRUD controller
    Implemented teams CRUD controller

--- EntityFrameworkCore.Api/DTOs/TeamReadDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EntityFrameworkCore.Api.DTOs
-{
-    public class TeamReadDto
-    {
-        public string Name { get; set; }
-        public string CoachName { get; set; }
-        public string LeagueName { get; set; }
-    }
-}
